d0d83f3c9b2075650d158f5b7c750a36dd1daf37
jhonny.andreyv <jhonny.andreyv@gmail.com>
2022-01-31 15:32:12 -0300

    Variaveis e Tipos

---
+++ Variáveis e Tipos/palindromo.js
@@ -0,0 +1,16 @@
+//Solução 1 
+function verificaPalindromo(string){
+    if(!string ) return "String Inexistente";
+    return string.split("").reverse().join("") === string;
+}
+
+// Solução 2
+function verificaPalindromo2(string){
+    if(!string ) return "String Inexistente";
+    for(let i = 0; i < string.length / 2; i++){
+        if(string[i]!== string[string.length - 1 - i]){
+            return false;
+        }
+    }
+    return true;
+}
