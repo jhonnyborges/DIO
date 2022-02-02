de331a180b49d5ce7b7e2b78d416de8b10645c53
jhonny.andreyv <jhonny.andreyv@gmail.com>
2022-02-02 11:58:25 -0300

    OOJS exercicio

---
+++ Orientação a Objetos/contaBancaria.js
@@ -0,0 +1,63 @@
+class contaBancaria {
+  constructor(agencia, numero, tipo, saldo){
+    this.agencia = agencia;
+    this.numero = numero;
+    this.tipo = tipo;
+    this._saldo = 0;
+  }
+  get saldo(){
+    return this._saldo;
+  }
+  set saldo(valor){
+    this._saldo = valor;
+  }
+
+  sacar(valor){
+    if (valor > this._saldo){
+      return "Operação Negada"
+    }
+    this._saldo = this._saldo - valor;
+    return this._saldo;
+  }
+
+  depositar(valor){
+    this._saldo = this._saldo + valor;
+    return this._saldo;
+  }
+}
+
+class contaCorrente extends contaBancaria {
+  constructor(agencia, numero, saldo, cartaoCredito){
+    super(agencia, numero, saldo);
+    this.tipo = 'corrente';
+    this.cartaoCredito = cartaoCredito;
+  }
+
+  get cartaoCredito (){
+    return this._cartaoCredito;
+  }
+
+  set cartaoCredito(valor){
+   this._cartaoCredito = valor;
+  }
+}
+
+class contaPoupanca extends contaBancaria{
+  constructor(agencia, numero, saldo){
+    super(agencia, numero, saldo);
+    this.tipo = 'poupanca';
+  }
+}
+
+class contaUniversitaria extends contaBancaria{
+  constructor(agencia, numero, saldo){
+    super(agencia, numero, saldo);
+    this.tipo = 'universitária';
+  }
+  sacar(valor){
+    if (valor > 500){
+      return "Operação Negada"
+    }
+    this._saldo = this._saldo - valor;
+  }
+}
